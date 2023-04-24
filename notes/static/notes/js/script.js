e54c685183b0af42bee606e137d233b12061ddff
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2023-04-24 11:02:50 +0200

    function for mobile nav

---
+++ notes/static/notes/js/script.js
@@ -0,0 +1,15 @@
+const mainMenu = document.querySelector(".mainMenu");
+const closeMenu = document.querySelector(".closeMenu");
+const openMenu = document.querySelector(".openMenu");
+
+const show = () => {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
+};
+
+const close = () => {
+  mainMenu.style.top = "-100%";
+};
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
