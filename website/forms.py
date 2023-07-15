4f25ee9949baa6d39fde0c577b20d521578b9c37
Jacek Ostański <jacek@ivolution.pl>
2023-07-15 11:56:35 +0200

    added consent

--- website/forms.py
+++ website/forms.py
@@ -2,14 +2,17 @@ from django import forms
 
 from .models import Client
 from django.utils.safestring import mark_safe
+from django.urls import reverse_lazy
 
 
 class ClientForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ClientForm, self).__init__(*args, **kwargs)
+        link = reverse_lazy("privacy_policy")
         self.fields["consent"].label = mark_safe(
-            "Wyrażam zgodę na przetwarzanie moich danych osobowych zgodnie z <a href=''>Polityką prywatności</a>"
+            f"Wyrażam zgodę na przetwarzanie moich danych osobowych zgodnie z <a href='{link}'>Polityką prywatności</a>"
         )
+        self.fields["consent"].label_suffix = ""
 
     class Meta:
         fields = ["name", "phone", "email", "activity", "consent"]
