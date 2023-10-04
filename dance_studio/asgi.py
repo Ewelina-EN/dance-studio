05ac96b526a78475ec77ac67b3fcfd4af923ea14
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2023-10-04 14:57:39 +0200

    rename

--- dance_studio/asgi.py
+++ dance_studio/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_in_one_hour project.
+ASGI config for dance_studio project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_in_one_hour.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dance_studio.settings")
 
 application = get_asgi_application()
--- dance_studio/wsgi.py
+++ dance_studio/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_in_one_hour project.
+WSGI config for dance_studio project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_in_one_hour.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dance_studio.settings")
 
 application = get_wsgi_application()
